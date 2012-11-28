4ddf6b5a8b18f693f22dbdd87d7fd2ffa9ab8dbb
Administrator <Administrator@DADI-PC>
2012-11-29 00:49:37 +0800

    11

--- src/com/mzone/oa/ui/MainActivity.java
+++ src/com/mzone/oa/ui/MainActivity.java
@@ -37,24 +37,15 @@ public class MainActivity extends FragmentActivity implements
 	static{
 		docBeans = new ArrayList<TodoDocumentBean>();
 		TodoDocumentBean tb = new TodoDocumentBean();
-		tb.title = "政协换届以来工作总结";
-		tb.time = "12-10-21 10:12:44";
-		tb.suggess = "已阅";
+		tb.title = " 富阳市人民政府关于富阳市农村村民建房管理的若干意见（试行）";
+		tb.time = "2012年11月06日";
+		tb.suggess = "请会签！  徐林  2012年11月28日10时32分";
 		tb.dengji = "等级1";
 		tb.desc = "今年以来，按照县委县政府的总体部署和县政协常委会工作要点安排，县政协认真履行工作";
-		tb.jinbanren = "陈国华";
+		tb.jinbanren = "徐林 ";
 		
 		docBeans.add(tb);
-		
-		tb = new TodoDocumentBean();
-		tb.title = "在党校学习贯彻十八大精神会议上的讲话";
-		tb.time = "12-10-22 11:12:44";
-		tb.suggess = "通过";
-		tb.dengji = "等级2";
-		tb.desc = "镇原县委党校学习贯彻党的十八大精神专题研讨会召开，6位教学骨干围绕精心准备的讲稿，先后发言";
-		tb.jinbanren = "李建国";
-		
-		docBeans.add(tb);
+	
 	}
 
 	private static final String STATE_MENUDRAWER = "com.mzone.oa.ui.MainActivity.menuDrawer";
